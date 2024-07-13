1188734193919554c86683f0f1d01ca04e270e51
kunalarya873 <kunalarya873@gmail.com>
2024-07-13 13:20:04 +0530

    Day 1 task completed

---
+++ day-1.js
@@ -0,0 +1,27 @@
+//Activity 1
+var a = 9;
+console.log(a)
+let b = 'hello';
+console.log(b);
+
+//Activity 2
+const c = true
+console.log(c)
+
+//Activity 3
+d = [1, 3]
+e =9
+f = 'ff'
+console.log(typeof(d),typeof(e),typeof(f))
+
+//Activity 4
+let g = 9
+console.log(g);
+g +=5
+console.log(g);
+
+//Activity 5
+const h = 10
+console.log(h);
+h =1
+console.log(h);

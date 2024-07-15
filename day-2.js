07fdf3c808b393bf84cc85003e450d19c1de2935
kunalarya873 <kunalarya873@gmail.com>
2024-07-15 10:33:22 +0530

    Day 2

---
+++ day-2.js
@@ -0,0 +1,35 @@
+// Activity 1
+let a = 2
+let b = 3
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a%b);
+
+//Activity 2
+let c = 2
+c += 3
+console.log(c);
+c-= 3
+console.log(c);
+
+//Activity 3
+let d =4 
+let e = 6
+console.log(d>e);
+console.log(d<e);
+console.log(d==e);
+console.log(d===e);
+
+//Activity 4
+let f = true
+let g =false
+console.log(f && g);
+console.log(f||g);
+console.log(!(f && g));
+
+//Activity 5
+const age = 26;
+const beverage = age >= 21 ? "Beer" : "Juice";
+console.log(beverage); // "Beer"
